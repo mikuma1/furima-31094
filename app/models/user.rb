f372162238b6860778454581900d72448234cd15
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-16 18:15:18 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A[a-z0-9]+\z/i}
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birth_date, numericality: { other_than: 0 }
+  end
 end
