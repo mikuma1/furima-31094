fa15d3f2f23d745bdaedbf76a1bbf58e9da8780d
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-19 19:34:52 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @categories = Category.all
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+  def item_params
+    params.reguire(:item).permit(:name, :info, :category, :sales_status, :shopping_fee_status, :prefecture, :scheduled_delivery, :price).merge(:user current_user_id)
+  end
 end
