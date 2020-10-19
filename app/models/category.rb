3556aeee88c043af47c84d472166d6714fa2e9fc
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-19 19:02:49 +0900

    categoryモデル作成

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+class Category < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '---'}
+    { id: , name: 'レディース'}
+    { id: , name: 'メンズ'}
+    { id: , name: 'ベビー・キッズ'}
+    { id: , name: 'インテリア・住まい・小物'}
+    { id: , name: '本・音楽・ゲーム'}
+    { id: , name: 'おもちゃ・ホビー・グッズ'}
+    { id: , name: '家電・スマホ・カメラ'}
+    { id: , name: 'スポーツ・レジャー'}
+    { id: , name: 'ハンドメイド'}
+    { id: , name: 'その他'}
+  ]
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :name, :info,:category_id,:sales_status_id,
+            :shopping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:user,
+            presence :true
 end
