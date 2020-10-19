a18dbc26b98927638459daa23825ea71dd10f519
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-19 19:35:28 +0900

    ActiveStrageのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  has_one_attached :image
 
   validates :name, :info,:category_id,:sales_status_id,
             :shopping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:user,
