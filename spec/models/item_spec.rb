5cb2dbd2bbab4a28574d71ce8135affc58b1a86c
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 17:06:32 +0900

    itemモデルの単体テスト作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品出品登録' do
+    context '商品登録がうまくいくとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品登録がうまくいかないとき' do
+      it 'nameが空の場合登録できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空の場合登録できない' do
+        @item.info = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'categoryが選択されていない場合登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'sales_statusが選択されていない場合登録できない' do
+        @item.sales_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status must be other than 1")
+      end
+      it 'shopping_fee_statusが選択されていない場合登録できない' do
+        @item.shopping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shopping fee status must be other than 1")
+      end
+      it 'prefectureが選択されていない場合登録できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'scheduled_deliveryが選択されていない場合登録できない' do
+        @item.scheduled_delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
+      end
+      it 'priceが空の場合登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300未満の場合登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it 'priceが9999999より多い場合登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it 'priceが半角数字でない場合登録できない' do
+        @item.price = '４００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
+
