f3a5e336d5fb4171c0e398b43dadc60ebef3ba62
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 11:43:34 +0900

    prefectureクラスをActiveHashで作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shopping_fee_status
+  belongs_to_active_hash :prefecture
   has_one_attached :image
 
   validates :name, :info, :category, :sales_status,
-            :shopping_fee_status,:prefecture_id,:scheduled_delivery_id,:price,:user,
+            :shopping_fee_status,:prefecture,:scheduled_delivery_id,:price,:user,
             presence: true
   with_options numericality: { other_than: 1 } do
-  validates :category_id
-  validates :sales_status_id
-  validates :sales_status_id
+    validates :category_id
+    validates :sales_status_id
+    validates :shopping_fee_status_id
+    validates :prefecture_id
   end
 
 end
