4effb1a26afd7098b9500ff3b827dfec8e2c7e1e
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 12:48:43 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,6 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-
+  validates :user_id, numericality: { greater_than_or_equal_to: 300, less_than: 9999999 },
+            format: { with: /\A[０-９]+\z/ }
 end
