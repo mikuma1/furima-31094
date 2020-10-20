a5026805412cb6be2625a721aef5dcfb5d356d23
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 18:55:41 +0900

    不要な記述のコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.order('creeated_at DESC')
+    # @items = Item.order('creeated_at DESC')
   end
 
   def new
