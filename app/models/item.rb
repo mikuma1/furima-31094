5ad038ff976cce38125ed1816568ab6631e42ba0
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 11:26:02 +0900

    sales_statusクラスをActiveHashで作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
   has_one_attached :image
 
-  validates :name, :info, :category, :sales_status_id,
+  validates :name, :info, :category, :sales_status,
             :shopping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:user,
             presence: true
   validates :category_id, numericality: { other_than: 1 }
+  validates :sales_status_id, numericality: { other_than: 1 }
+
 end
