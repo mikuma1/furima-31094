454589eacc96cf24a02a7f98de2eeca4e9c586a6
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 11:35:01 +0900

    shopping_fee_statusクラスをActimehashで作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shopping_fee_status
   has_one_attached :image
 
   validates :name, :info, :category, :sales_status,
-            :shopping_fee_status_id,:prefecture_id,:scheduled_delivery_id,:price,:user,
+            :shopping_fee_status,:prefecture_id,:scheduled_delivery_id,:price,:user,
             presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :sales_status_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+  validates :category_id
+  validates :sales_status_id
+  validates :sales_status_id
+  end
 
 end
