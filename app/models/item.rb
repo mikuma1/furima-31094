82b5a1142e06ff0768fc64b73274cb9851cfcb92
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 12:05:52 +0900

    scheduled_deliveryクラスをActivehashで作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shopping_fee_status
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
   has_one_attached :image
 
   validates :name, :info, :category, :sales_status,
-            :shopping_fee_status,:prefecture,:scheduled_delivery_id,:price,:user,
+            :shopping_fee_status,:prefecture,:scheduled_delivery,:price,:user,
             presence: true
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :sales_status_id
     validates :shopping_fee_status_id
     validates :prefecture_id
+    validates :scheduled_delivery_id
   end
 
 end
