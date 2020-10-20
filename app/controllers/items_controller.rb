03a7e795ee17337ede58087fd55a9f8ecd555d1d
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-20 11:15:26 +0900

    ルーティングとコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("creeated_at DESC")
   end
 
   def new
-    @categories = Category.all
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,6 +24,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.reguire(:item).permit(:name, :info, :category, :sales_status, :shopping_fee_status, :prefecture, :scheduled_delivery, :price).merge(:user current_user_id)
+    params.reguire(:item).permit(:name, :info, :category, :sales_status, :shopping_fee_status, :prefecture, :scheduled_delivery, :price).merge(user: current_user_id)
   end
 end
