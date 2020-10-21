89f4fa6253ba4bc5a109174d79b0fd90a03b0120
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-21 19:18:04 +0900

    before_actionへの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     item_params[:image] = @item.image if item_params[:image].nil?
     if @item.update(item_params)
       redirect_to root_path
