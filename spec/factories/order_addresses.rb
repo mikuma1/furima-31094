9ba904f2073be7560990ad25816008a38e96f319
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-22 18:12:08 +0900

    order_addressesモデルの単体テスト作成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :order_address do
-    
+    user_id { 1 }
+    item_id { 1 }
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }
+    addresses { '1-1' }
+    building { '東京ハイツ' }
+    phone_number { '09011111111' }
   end
 end
