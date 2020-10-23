6c5c7d38ff06384a799c4ac70ea633575523f4e7
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-23 12:48:03 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,14 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(address_params)
+    price = Item.find(address_params[:item_id]).price
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: price,
+        card: address_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -18,6 +25,6 @@ class OrdersController < ApplicationController
   private
 
   def address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,11 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :user_id, :item_id, :city, :addresses
+    validates :user_id, :item_id, :city, :addresses, :token
     validates :phone_number, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/ }
   end
 
