fe8137203c20e24d8045e4ae04ac6bcf90531b8f
mikuma1 <72071661+mikuma1@users.noreply.github.com>
2020-10-23 14:32:45 +0900

    ページ遷移の制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -26,5 +29,10 @@ class OrdersController < ApplicationController
 
   def address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user == @item.user || @item.order.present?
   end
 end
